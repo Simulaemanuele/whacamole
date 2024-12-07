3d41a7306e2f4a11f10af28a5f9d060c4f2092eb
Emanuele Simula <simulaemanuele@gmail.com>
2024-12-07 16:21:27 +0100

    feat: first implementation of WhacAMole class, panel opening successfully

--- src/WhacAMole.java
+++ src/WhacAMole.java
@@ -1,3 +1,21 @@
-public class WhacAMole {
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
 
+import javax.swing.*;
+
+public class WhacAMole {
+    int boardWidth = 600;
+    int boardHeight = 650; // 50px for the top text panel
+
+    JFrame frame = new JFrame("Mario: Whac A Mole");
+
+    WhacAMole() {
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+    }
 }
